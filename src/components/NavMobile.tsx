3fe56f2e93ba2b4a7f6975c4094ce1cb2aba8d2b
Son Chaewon <good6637@naver.com>
2024-05-14 10:58:12 +0900

    first commit

---
+++ src/components/NavMobile.tsx
@@ -0,0 +1,29 @@
+// import icons
+import { IoMdClose } from "react-icons/io";
+
+const NavMobile = ({ setNavMobile }: any) => {
+  return (
+    <nav className="lg:hidden bg-[#251f3f] w-full h-full">
+      <IoMdClose
+        onClick={() => setNavMobile(false)}
+        className="text-3xl absolute left-4 top-6 cursor-pointer"
+      />
+      <ul className="flex flex-col items-center justify-center space-y-8 h-full font-secondary">
+        <li className="text-lg  ">
+          <a href="#">Home</a>
+        </li>
+        <li className="text-lg  ">
+          <a href="#">Company</a>
+        </li>
+        <li className="text-lg  ">
+          <a href="#">Features</a>
+        </li>
+        <li className="btn">
+          <a href="#">Sign up</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavMobile;
